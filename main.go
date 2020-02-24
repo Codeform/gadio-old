00002828bbc0d391f289fdef389e20094acbeb3e
Codeform <pagini_yueu@outlook.com>
2020-02-24 15:51:35 +0800

    Generator changed to podcast libs from eduncan911

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 	re "regexp"
@@ -9,32 +10,39 @@ import (
 	"sync"
 	"time"
 
-	"github.com/beevik/etree"
+	"github.com/eduncan911/podcast"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/debug"
-	sl "github.com/sean-public/fast-skiplist"
 )
 
 const (
-	base      = "https://www.gcores.com/"
-	domain    = "www.gcores.com"
-	fmtPubIn  = "2006-01-02"
-	fmtPubOut = "Mon, 02 Jan 2006 15:04:05 +0000"
+	base     = "https://www.gcores.com/"
+	domain   = "www.gcores.com"
+	fmtPubIn = "2006-01-02"
 )
-
-type closure struct {
-	publish, //
-	cover, //
-	duration, //
-	catalog, //
-	title, //
-	summary, //
-	audio string
-}
 
 func main() {
 	logs, _ := os.Create("./results/log.txt")
 	defer logs.Close()
+
+	now := time.Now()
+	then := time.Date(2010, time.May, 10, 12, 00, 00, 00, now.Location())
+
+	feed := podcast.New("（非官方源）GADIO",
+		base,
+		"（非官方源）机核网 gcores.com 「不止是游戏」，机核从年轻人的兴趣出发，旨在用亲切幽默地风格在游戏、电影、音乐、消费等不同领域提供有价值的精致内容，把有着各种兴趣爱好的年轻人聚集在一起，在这个无趣的世界里，找到一处属于自己的安宁之乡。",
+		&then,
+		&now)
+	feed.Copyright = domain
+	feed.Generator = "custom golang application based on github.com/eduncan911/podcast"
+	feed.Language = "zh-cn"
+	feed.SkipDays = "30"
+	feed.ManagingEditor = "gamecores@qq.com (机核网 www.gcores.com)"
+	feed.AddCategory("Games & Hobbies", []string{"Video Games"})
+	feed.AddAuthor("机核网 www.gcores.com", "gamecores@qq.com")
+	feed.AddSubTitle("机核网－嘉电游GADIO")
+	feed.AddImage("http://media.fmit.cn/feed/gadionewlogos.png")
+	feed.AddCategory("Games & Hobbies", []string{"Video Games"})
 
 	ptnPage := re.MustCompile("/radios\\?page=\\d*$")
 	ptnClosure := re.MustCompile("/radios/\\d*$")
@@ -50,16 +58,6 @@ func main() {
 		}),
 	)
 
-	// if prx, err := proxy.RoundRobinProxySwitcher(
-	// 	"http://171.35.212.106:9999",
-	// 	"http://123.169.102.37:9999",
-	// 	"http://110.243.8.36:9999",
-	// 	"http://118.212.106.252:9999",
-	// 	"http://163.204.247.170:9999",
-	// ); err == nil {
-	// 	pageCrawler.SetProxyFunc(prx)
-	// }
-
 	pageCrawler.Limit(&colly.LimitRule{
 		Delay:       time.Microsecond * 800,
 		RandomDelay: time.Microsecond * 600,
@@ -68,8 +66,11 @@ func main() {
 
 	closureCrawler := pageCrawler.Clone()
 
-	closures := sl.New()
 	var lck sync.Mutex
+
+	pageCrawler.OnRequest(func(req *colly.Request) {
+		log.Println("Visiting page", req.URL)
+	})
 
 	pageCrawler.OnHTML("div.col-xl-3.col-md-4.col-sm-6", func(e *colly.HTMLElement) {
 		pageCtx := colly.NewContext()
@@ -82,7 +83,7 @@ func main() {
 					if pub, err := time.Parse(fmtPubIn, coe.Text); err == nil {
 						pageCtx.Put("publish", pub)
 					} else {
-						pageCtx.Put("publish", time.Now())
+						pageCtx.Put("publish", now)
 					}
 				}
 			case "original_imgArea":
@@ -134,19 +135,12 @@ func main() {
 	})
 
 	closureCrawler.OnScraped(func(rsp *colly.Response) {
-		key := float64(rsp.Ctx.GetAny("publish").(time.Time).Unix()) * -1
 		lck.Lock()
-		closures.Set(key,
-			&closure{
-				publish:  rsp.Ctx.GetAny("publish").(time.Time).Format(fmtPubOut),
-				cover:    rsp.Ctx.Get("cover"),
-				duration: rsp.Ctx.Get("duration"),
-				catalog:  rsp.Ctx.Get("catalog"),
-				title:    escape(rsp.Ctx.Get("title")),
-				summary:  escape(rsp.Ctx.Get("summary")),
-				audio:    rsp.Ctx.Get("audio"),
-			})
+		item := createFeedItem(rsp.Ctx)
+		feed.AddItem(item)
 		lck.Unlock()
+
+		log.Println("Return with scrapped podcast", item.PubDate, ":", item.Title)
 	})
 
 	pageCrawler.Visit(base + "radios")
@@ -154,126 +148,71 @@ func main() {
 	pageCrawler.Wait()
 	closureCrawler.Wait()
 
-	rsltXml := etree.NewDocument()
-
-	for cursor := closures.Front(); cursor != nil; cursor = cursor.Next() {
-		if c := cursor.Value().(*closure).Marshal(); c != nil {
-			rsltXml.AddChild(c)
-		}
-	}
 	results, _ := os.Create("./results/gadio.xml")
-	rsltXml.IndentTabs()
-	holder, _ := rsltXml.WriteToString()
-	results.WriteString(`<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0"
-	xmlns:content="http://purl.org/rss/1.0/modules/content/"
-	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-	xmlns:dc="http://purl.org/dc/elements/1.1/"
-	xmlns:atom="http://www.w3.org/2005/Atom"
-	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
-	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
-	xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
-xmlns:rawvoice="http://www.rawvoice.com/rawvoiceRssModule/"
->
-
-<channel>
-	<title>机核网 GADIO 游戏广播（非官方源）</title>
-	<atom:link href="https://pagini-engine.com/feed/gadio.xml" rel="self" type="application/rss+xml" />
-	<link>https://pagini-engine.com/feed/gadio.xml</link>
-	<description>机核网 www.gcores.com</description>
-	<lastBuildDate>` + time.Now().Format(fmtPubOut) + `</lastBuildDate>
-
-	<language>zh-cn</language>
-	<sy:updatePeriod>monthly</sy:updatePeriod>
-	<sy:updateFrequency>30</sy:updateFrequency>
-	<generator>dj-podcaster</generator>
-	<itunes:summary>机核网 gcores.com 「不止是游戏」</itunes:summary>
-	<itunes:author>机核网 www.gcores.com</itunes:author>
-
-	<itunes:explicit>clean</itunes:explicit>
-	<itunes:image href="http://media.fmit.cn/feed/gadionewlogos.png" />
-	<itunes:owner>
-		<itunes:name>机核网 www.gcores.com</itunes:name>
-		<itunes:email>gamecores@qq.com</itunes:email>
-	</itunes:owner>
-	<managingEditor>gamecores@qq.com (机核网 www.gcores.com)</managingEditor>
-
-	<copyright>www.gcores.com</copyright>
-	<itunes:subtitle>机核网－嘉电游GADIO</itunes:subtitle>
-	<itunes:keywords>游戏,Xbox360,PS3,Wii,3DS,PSP2,宅男,机核网,游戏电台,心得,攻略,采访,游戏论坛</itunes:keywords>
-	<!--itunes:category text="Games &amp; Hobbies">
-		<itunes:category text="Video Games" />
-	</itunes:category-->
-	<itunes:category text="Games &amp; Hobbies">
-	<itunes:category text="Video Games" />
-</itunes:category>
-	` + holder + `
-	</channel>
-	</rss>
-	`)
-
+	feed.Encode(results)
 	results.Close()
 }
 
-func escape(str string) string {
-	str = strings.ReplaceAll(str, "&", "&amp")
-	str = strings.ReplaceAll(str, ">", "&gt")
-	str = strings.ReplaceAll(str, "<", "&lt")
-	str = strings.ReplaceAll(str, "'", "&apos")
-	str = strings.ReplaceAll(str, "\"", "&quot")
-	return str
+func createFeedItem(ctx *colly.Context) podcast.Item {
+	var item podcast.Item
+	rsp, err := http.Get(ctx.Get("audio"))
+	if err != nil {
+		return item
+	}
+	defer rsp.Body.Close()
+
+	pub := ctx.GetAny("publish").(time.Time)
+	length, _ := strconv.Atoi(rsp.Header["Content-Length"][0])
+
+	item.Title = ctx.Get("title") + " | " + ctx.Get("catalog")
+
+	item.Description = ctx.Get("summary")
+
+	item.Link = domain
+
+	item.AddImage(ctx.Get("cover"))
+
+	item.AddPubDate(&pub)
+
+	item.AddEnclosure(ctx.Get("audio"), func(str string) podcast.EnclosureType {
+		switch str {
+		case "audio/x-m4a":
+			return podcast.M4A
+		case "video/x-m4v":
+			return podcast.M4V
+		case "video/mp4":
+			return podcast.MP4
+		case "audio/mpeg":
+			return podcast.MP3
+		case "video/quicktime":
+			return podcast.MOV
+		case "application/pdf":
+			return podcast.PDF
+		case "document/x-epub":
+			return podcast.EPUB
+		default:
+			return -1
+		}
+	}(rsp.Header["Content-Type"][0]), int64(length))
+
+	item.AddDuration(parseDuration(ctx.Get("duration")))
+
+	return item
 }
 
-func (cl *closure) Marshal() *etree.Element {
-	elem := etree.NewElement("item")
-	var iter *etree.Element
-
-	iter = elem.CreateElement("title")
-	iter.SetText(cl.title + " | " + cl.catalog)
-	// iter.SetText("sssss" + " | " + cl.catalog)
-
-	iter = elem.CreateElement("pubDate")
-	iter.SetText(cl.publish)
-
-	iter = elem.CreateElement("itunes:duration")
-	iter.SetText(cl.duration)
-
-	iter = elem.CreateElement("guid")
-	iter.CreateAttr("isPermaLink", "false")
-	iter.SetText(cl.audio)
-
-	iter = elem.CreateElement("description")
-	iter.SetText("<![CDATA[" + cl.summary + "]]>")
-
-	iter = elem.CreateElement("wfw:commentRss")
-	iter.SetText("http://feed.tangsuanradio.com/gadio.xml")
-
-	iter = elem.CreateElement("slash:comments")
-	iter.SetText("0")
-
-	iter = elem.CreateElement("itunes:image")
-	iter.SetText(cl.cover)
-
-	iter = elem.CreateElement("enclosure")
-	rsp, err := http.Head(cl.audio)
-	if err != nil {
-		return nil
+func parseDuration(str string) int64 {
+	if str == "" {
+		return 3600 + 22*60 + 18
 	}
-	iter.CreateAttr("url", cl.audio)
-	iter.CreateAttr("length", rsp.Header["Content-Length"][0])
-	iter.CreateAttr("type", rsp.Header["Content-Type"][0])
-
-	iter = elem.CreateElement("itunes:subtitle")
-	iter.SetText(cl.summary)
-
-	iter = elem.CreateElement("itunes:summary")
-	iter.SetText(cl.summary)
-
-	iter = elem.CreateElement("itunes:author")
-	iter.SetText("www.gcores.com")
-
-	iter = elem.CreateElement("itunes:explicit")
-	iter.SetText("clean")
-
-	return elem
+	var dur int64
+	seq := strings.Split(str, ":")
+	t, _ := strconv.Atoi(seq[0])
+	dur += int64(t) * 3600
+	t, _ = strconv.Atoi(seq[1])
+	dur += int64(t) * 60
+	if len(seq) == 3 {
+		t, _ = strconv.Atoi(seq[2])
+	}
+	dur += int64(t)
+	return dur
 }
